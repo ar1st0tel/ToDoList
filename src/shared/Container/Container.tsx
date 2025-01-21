62bf2190dd62b2f728bdec8f44d0affb8c54714c
Egor <Shambasovegor@yandex.ru>
2025-01-21 18:19:22 +0400

    can add and remove tasks

--- src/shared/Container/Container.tsx
+++ src/shared/Container/Container.tsx
@@ -1,15 +1,14 @@
 import classes from "./Container.module.scss"
-import {Paragraph} from "../Paragraph/Paragraph.tsx";
-import {InputElem} from "../InputElem/InputElem.tsx";
+import {TaskComponent} from "../../features/TaskList/TaskList.tsx";
+
 
 
 const Container = () => {
     return (<div className={classes.container}>
         <div className={classes.title}>todos </div>
                 <div className={classes.input}>
-                    <InputElem />
+                    <TaskComponent />
                 </div>
-                <Paragraph />
         </div>
     )
 }
